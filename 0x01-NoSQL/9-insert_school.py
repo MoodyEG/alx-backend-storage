894ef33c054eed29ea1290e0ce6a8555c7d89fa1
MoodyEG <moodyeg2011@yahoo.com>
2024-08-13 01:56:52 +0300

    Testing 9

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Inserts a new document in a collection """
+
+def insert_school(mongo_collection, **kwargs):
+    """Inserts a new document in a collection"""
+    ins = mongo_collection.insert_one(kwargs)
+    return ins.inserted_id
