b97629e70c36d5ae2f212279fb50673e5d76a478
MoodyEG <moodyeg2011@yahoo.com>
2024-08-13 06:40:03 +0300

    Testing Adv 101

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Top students """
+
+
+def top_students(mongo_collection):
+    """ Returns all students sorted by average score """
+    students = list(mongo_collection.find())
+    for student in students:
+        total_score = sum([topic["score"] for topic in student["topics"]])
+        student["averageScore"] = total_score / len(student["topics"])
+    return sorted(students, key=lambda student: student["averageScore"], reverse=True)
