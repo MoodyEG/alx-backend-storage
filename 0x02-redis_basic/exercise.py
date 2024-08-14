9423605a1c4edc21da4e70b379df1c98e0cd02c8
MoodyEG <moodyeg2011@yahoo.com>
2024-08-14 13:50:53 +0300

    Testing 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@
 
 import redis
 import uuid
-from typing import Any, Union
+from typing import Any, Union, Callable
 
 
 class Cache:
@@ -20,3 +20,22 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Union[Callable, None] = None) -> Any:
+        """ Get the data in the desired format """
+        data = self._redis.get(key)
+        if not data:
+            return None
+
+        if fn:
+            return fn(data)
+
+        return data
+
+    def get_str(self, key: str) -> str:
+        """ Get the data as a string """
+        return self.get(key, lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """ Get the data as an integer """
+        return self.get(key, int)
